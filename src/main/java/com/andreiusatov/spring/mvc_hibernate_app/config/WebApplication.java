1988920bb2b19445ac9d702145b64d89893e1f7c
Андрей Усатов <andrei_usatow@mail.ru>
2025-06-18 16:55:49 +0500

    Задача 3.1.1

---
+++ src/main/java/com/andreiusatov/spring/mvc_hibernate_app/config/WebApplication.java
@@ -0,0 +1,13 @@
+package com.andreiusatov.spring.mvc_hibernate_app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = "com.andreiusatov.spring.mvc_hibernate_app")
+@EntityScan(basePackages = "com.andreiusatov.spring.mvc_hibernate_app.model")
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
